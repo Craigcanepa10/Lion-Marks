d66709961e4c1d68c4702ff370b66f864220d90c
Craig Canepa <craigcanepa10@gmail.com>
2021-10-19 17:23:32 -0500

    Initial Commit

---
+++ Lion Marks/Lion_MarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lion_MarksApp.swift
+//  Lion Marks
+//
+//  Created by Craig Canepa on 10/19/21.
+//
+
+import SwiftUI
+
+@main
+struct Lion_MarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
